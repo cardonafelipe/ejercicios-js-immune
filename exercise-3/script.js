75546893738316c82db02c920ade19ffa2a7c786
Felipe Cardona <felipecardonadenis98@gmail.com>
2024-12-29 18:33:47 +0100

    Fix exercise 3

--- exercise-3/script.js
+++ exercise-3/script.js
@@ -6,4 +6,8 @@ function render(content) {
 const weight = prompt("Introduce el peso (kg)"); // paso 1
 const height = prompt("Introduce la altura (m)"); // paso 2
 const imc = weight/(height**2) // paso 3
-render(`<p>El resultado es: ${imc}</p>`);
+if (imc >= 35 && imc < 40) {
+ render(`<p>Eres Obesidad de clase II, tu IMC es: ${imc}</p>`);
+} else {
+ render(`<p>No eres Obeso de clase II, tu IMC es: ${imc} </p>`);
+};
